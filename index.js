643036a46d3ef148863af06b8973d2507100727c
Ghulam Mustafa <57095619+mustafa-mehboob@users.noreply.github.com>
2022-11-27 01:25:43 +0500

    Update

--- index.js
+++ index.js
@@ -1,48 +1,70 @@
+#! /usr/bin/env node
 import inquirer from 'inquirer';
 import Chalk from 'chalk';
-// let chalkRainbow = require('chalk-rainbow')
-console.log(Chalk.italic.bold.cyanBright('Welcome to Ghulam Mustafa calculator'));
-let value = await inquirer.prompt([
-    {
-        name: "sign",
-        type: 'list',
-        message: 'Plase slect what you want',
-        choices: [
-            "Addition",
-            "Multiplication",
-            "Division",
-            "Subtraction"
-        ],
-    },
-    {
-        name: "value1",
-        type: "number",
-        message: "Enter a number",
-    },
-    {
-        name: "value2",
-        type: "number",
-        message: "Enter a second number",
-    }
-]);
-(() => {
-    if (!value.value1 || !value.value2) {
-        console.log(Chalk.redBright("Please enter number only"));
-    }
-    else {
-        switch (value.sign) {
-            case "Addition":
-                console.log(Chalk.blueBright(value.value1 + value.value2));
-                break;
-            case "Multiplication":
-                console.log(Chalk.greenBright(value.value1 * value.value2));
-                break;
-            case "Division":
-                console.log(Chalk.blueBright(value.value1 / value.value2));
-                break;
-            default:
-                console.log(Chalk.yellowBright(value.value1 - value.value2));
-                break;
+import chalk from 'chalk';
+const welMsg1 = `╦ ╦┌─┐┬  ┬  ┌─┐┌─┐┌┬┐  ┌┬┐┌─┐  ┌┬┐┬ ┬┌─┐┌┬┐┌─┐┌─┐┌─┐  ┌─┐┌─┐┬  ┌─┐┬ ┬┬  ┌─┐┌┬┐┌─┐┬─┐`;
+const welMsg2 = `║║║├┤ │  │  │  │ ││││   │ │ │  ││││ │└─┐ │ ├─┤├┤ ├─┤  │  ├─┤│  │  │ ││  ├─┤ │ │ │├┬┘`;
+const welMsg3 = `╚╩╝└─┘┴─┘┴─┘└─┘└─┘┴ ┴   ┴ └─┘  ┴ ┴└─┘└─┘ ┴ ┴ ┴└  ┴ ┴  └─┘┴ ┴┴─┘└─┘└─┘┴─┘┴ ┴ ┴ └─┘┴└─`;
+console.log(`
+${chalk.redBright(welMsg1)}
+${chalk.blueBright(welMsg2)}
+${chalk.redBright(welMsg3)}
+`);
+let func = async () => {
+    let value = await inquirer.prompt([
+        {
+            name: "sign",
+            type: 'list',
+            message: 'Plase slect what you want',
+            choices: [
+                "Addition",
+                "Multiplication",
+                "Division",
+                "Subtraction"
+            ],
+        },
+        {
+            name: "value1",
+            type: "number",
+            message: "Enter first value",
+        },
+        {
+            name: "value2",
+            type: "number",
+            message: "Enter second value",
         }
-    }
-})();
+    ]);
+    (() => {
+        if (!value.value1 || !value.value2) {
+            console.log(Chalk.redBright("Please enter number only"));
+        }
+        else {
+            switch (value.sign) {
+                case "Addition":
+                    console.log(Chalk.blueBright(`${value.value1} + ${value.value2} = ${value.value1 + value.value2}`));
+                    break;
+                case "Multiplication":
+                    console.log(Chalk.greenBright(`${value.value1} * ${value.value2} = ${value.value1 * value.value2}`));
+                    break;
+                case "Division":
+                    console.log(Chalk.blueBright(`${value.value1} / ${value.value2} = ${value.value1 / value.value2}`));
+                    break;
+                default:
+                    console.log(Chalk.yellowBright(`${value.value1} - ${value.value2} = ${value.value1 - value.value2}`));
+                    break;
+            }
+        }
+    })();
+};
+let repeatFunc = async () => {
+    do {
+        await func();
+        var repeat = await inquirer.prompt({
+            type: "input",
+            name: "restart",
+            message: "Do you want to continue"
+        });
+        // console.log(repeat.restart.toLowerCase());
+    } while (repeat.restart.toLowerCase() === "y" || repeat.restart.toLowerCase() === "yes");
+};
+repeatFunc();
